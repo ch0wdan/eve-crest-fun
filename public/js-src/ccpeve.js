d3e0bf33bd6cda14fa50f09c1cffcec8015dd389
Les Orchard <me@lmorchard.com>
2015-03-31 01:58:25 -0400

    Start playing with building an IGB web app

---
+++ public/js-src/ccpeve.js
@@ -0,0 +1,29 @@
+if (typeof CCPEVE == 'undefined') {
+
+    var CCPEVE = window.CCPEVE = {
+        mock: true
+    };
+
+    /*
+    CCPEVE.showMarketDetails = function (type_id) {
+        window.location.href = '/market/type/' + type_id;
+    };
+    */
+
+    var stub_names = [
+        'requestTrust',
+        'showMarketDetails',
+        'showInfo'
+    ];
+
+    for (var i=0; i<stub_names.length; i++) {
+        (function (name) {
+            if (name in CCPEVE) { return; }
+            CCPEVE[name] = function () {
+                var args = JSON.stringify(Array.prototype.slice.apply(arguments));
+                console.log("CCPEVE."+name+" "+args);
+            }
+        })(stub_names[i]);
+    }
+
+}
